633d061572665b2f052b3ec0ca0b9a6fc5c0da1d
chizycodes <okwudirejoy@gmail.com>
2022-11-06 01:44:56 +0100

    feat: route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import './App.css';
+import UsersList from './pages/usersList';
+import Error404 from './pages/error404';
+import { Route, Routes } from 'react-router-dom';
 
-
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+export default function App() {
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<UsersList />}>
+					<Route path="users" element={<UsersList />} />
+				</Route>
+				<Route path="*" element={<Error404 />} />
+			</Routes>
+		</div>
+	);
 }
-
-export default App;
