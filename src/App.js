5a46a9f9de6796ebb70cf3638d4915809f963a5d
chizycodes <okwudirejoy@gmail.com>
2022-11-06 02:01:38 +0100

    fix: routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import './App.css';
 import UsersList from './pages/usersList';
+import Home from './pages/home';
 import Error404 from './pages/error404';
+import Login from './pages/login';
+import Nav from './components/Nav';
+import ErrorBoundaryTest from './pages/error-boundary';
+
+import { AuthContextProvider } from './context/AuthContext';
 import { Route, Routes } from 'react-router-dom';
+import PrivateRoutes from './routes/PrivateRoutes';
 
 export default function App() {
 	return (
 		<div>
-			<Routes>
-				<Route path="/" element={<UsersList />}>
-					<Route path="users" element={<UsersList />} />
-				</Route>
-				<Route path="*" element={<Error404 />} />
-			</Routes>
+			<AuthContextProvider>
+				<Nav />
+				<Routes>
+					<Route path="/" element={<Home />} />
+
+					<Route path="login" element={<Login />} />
+					<Route path="error-boundary" element={<ErrorBoundaryTest />} />
+					<Route
+						path="users"
+						element={
+							<PrivateRoutes>
+								<UsersList />
+							</PrivateRoutes>
+						}
+					/>
+					<Route path="*" element={<Error404 />} />
+				</Routes>
+			</AuthContextProvider>
 		</div>
 	);
 }
